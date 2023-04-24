f51b3a712059a428bbdaa2442cbf849281597318
magnetenstad <tenstad.magne@gmail.com>
2023-04-24 19:19:02 +0200

    Implement lexer from dragon book

---
+++ lexer.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+)
+
+type Token struct {
+    tag int
+    value int
+    lexeme string
+}
+
+type Tag int
+const (
+    Zero Tag = 0
+    NumTag Tag = iota + 256
+    IdTag
+    TrueTag
+    FalseTag
+)
+
+type Lexer struct {
+    line int
+    peek rune
+    words map[string]Token
+    reader io.RuneReader
+}
+
+func (lexer *Lexer) scan() (*Token, error) {
+    var err error = nil
+    stop := false
+    for !stop {
+        stop, err = lexer.peekNext()
+        if lexer.peek == ' ' || lexer.peek == '\t' { continue }
+        if lexer.peek == '\n' { 
+            lexer.line += 1
+            continue
+        }
+        break
+    }
+
+    if (err != nil) {
+        return nil, err
+    }
+
+    if unicode.IsDigit(lexer.peek) {
+        value := 0
+        
+        for unicode.IsDigit(lexer.peek) {
+            value = value * 10 + int(lexer.peek)
+            lexer.peekNext()
+        }
+        
+        return &Token { int(NumTag), value, "" }, nil
+    }
+
+    if unicode.IsLetter(lexer.peek) {
+        var sb strings.Builder
+
+        for unicode.IsLetter(lexer.peek) {
+            sb.WriteRune(lexer.peek)
+            lexer.peekNext()
+        }
+        
+        lexeme := sb.String()
+        word := lexer.words[lexeme]
+
+        if (word.tag != 0) { return &word, nil }
+        t := Token { int(IdTag), 0, lexeme }
+        lexer.words[lexeme] = t
+        
+        return &t, nil
+    }
+
+    t := Token { int(lexer.peek), 0, string(lexer.peek) }
+    lexer.peek = ' '
+    return &t, nil
+}
+
+func (lexer *Lexer) scanAll() []Token {
+    var tokens []Token
+    for true {
+        token, err := lexer.scan()
+        fmt.Println(token)
+        if err != nil {
+            break
+        }
+        tokens = append(tokens, *token)
+    }
+    return tokens
+}
+
+func (lexer *Lexer) peekNext() (bool, error) {
+    peek, _, err := lexer.reader.ReadRune()
+    if err != nil { 
+        return true, err
+    }
+    lexer.peek = peek
+    return false, nil
+}
+
+func (lexer *Lexer) reserve(token Token) {
+    lexer.words[token.lexeme] = token
+}
+
+func newLexer(reader io.RuneReader) Lexer {
+    lexer := Lexer { 0, ' ', make(map[string]Token), reader }
+    lexer.reserve(Token { int(TrueTag), 1, "true" })
+    lexer.reserve(Token { int(FalseTag), 0, "false" })
+    return lexer
+}
--- main.go
+++ main.go
@@ -1,89 +1,21 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
-	"regexp"
-	"strings"
 )
-
-func check(e error) {
-    if e != nil {
-        panic(e)
-    }
-}
-
-type TokenType string
-const (
-    Unknown TokenType = ""
-    Quote = "\""
-    NewLine = "\\n"
-    Space = " "
-    Dash = "-"
-    Word = "\\S+"
-)
-
-var tokenTypes = []TokenType {
-    Unknown,
-    Quote,
-    NewLine,
-    Space,
-    Dash,
-    Word,
-}
-
-type Token struct {
-    data string
-    tokenType TokenType
-}
 
 func main() {
+	file, err := os.Open("lexme.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
 
-    file, err := os.ReadFile("example.txt")
-    check(err)
+	reader := bufio.NewReader(file)
+	lexer := newLexer(reader)
+	tokens := lexer.scanAll()
 
-    tokens := lex(file)
-
-    fmt.Println(tokens)
-}
-
-func lex(bytes []byte) []Token {
-    
-    var tokens []Token
-    var sb strings.Builder
-    var token Token
-
-    for _, b := range bytes {
-        sb.WriteByte(b)
-
-        tokenType := Unknown
-        for _, t := range tokenTypes {
-            matched, err := regexp.MatchString(
-                fmt.Sprintf("^%s$", t), sb.String())
-            check(err)
-
-            if matched {  
-                tokenType = t
-                break
-            }
-        }
-        
-        if tokenType == Unknown {
-            tokens = append(tokens, token)
-            sb.Reset()
-            continue
-        }
-
-        token.tokenType = tokenType
-        token.data = sb.String()
-
-        if (tokenType != Word) {
-            tokens = append(tokens, token)
-            sb.Reset()
-        }
-    }
-    tokens = append(tokens, token)
-    sb.Reset()
-    
-    return tokens
+	fmt.Println(tokens)
 }
